219c8748e59c57603ffa6cbb41ebc97363bd379c
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-24 20:49:25 +0300

    Time: 183 ms (41.2%), Space: 23.2 MB (17.57%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,34 +1,53 @@
+class Node:
+    def __init__(self, key: int):
+        self.key = key
+        self.next = None
+
+
 class MyHashSet:
 
     def __init__(self):
-        self.data = [[]] * 10
+        self.set = [Node(0) for _ in range(10**4)]
 
-    def hash(self,key: int) -> int:
-        return key % 10
+    def hash(self, key: int) -> Node:
+        index = key % len(self.set)
+        return self.set[index]
 
     def add(self, key: int) -> None:
-        if self.contains(key):
-            return
-        else:
-            list = self.data[self.hash(key)]
-            list.append(key)
+        head = self.hash(key)
+        while head.next is not None:
+            if head.next.key == key:
+                return
+            else:
+                head = head.next
+        head.next = Node(key)
+        return
 
     def remove(self, key: int) -> None:
-        if self.contains(key):
-            list = self.data[self.hash(key)]
-            # index = list.index(key)
-            list.remove(key)
-            return True
-        else:
-            return False
+        head =  self.hash(key).next
+        prev = None
+        while head:
+            if head is None:
+                return
+            elif head.key == key:
+                if prev is None:
+                    index = self.hash(key)
+                    index.next = head.next
+                else:
+                    prev.next = head.next
+                return
+            prev = head
+            head = head.next
+        
 
     def contains(self, key: int) -> bool:
-        index = self.hash(key)
-        list = self.data[index]
-        if len(list):
-            return key in list
-        else:
-            return False
+        head = self.hash(key)
+        head = head.next
+        while head:
+            if head.key == key:
+                return True
+            head = head.next
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
