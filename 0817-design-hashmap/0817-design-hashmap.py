74b5b789b191c9ef4c0bd0126094b0a296d90bfb
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-25 17:38:53 +0300

    Time: 183 ms (59.76%), Space: 19.9 MB (32.67%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,36 +1,49 @@
-# class Node:
-#     def __init_(self,input):
-#         key , value =  input
-#         self.value = value
-#         self.key = key
-#         self.next = None
+class NodeList:
+    def __init__(self, key, value):
+        self.value = value
+        self.key = key
+        self.next = None
+
 
 class MyHashMap:
 
     def __init__(self):
-        self.map = [[] for _ in range(10**6)]
+        self.map = [NodeList(-1, -1) for _ in range(1000)]
 
-    def hash(self,key:int)-> int:
+    def hash(self, key: int) -> int:
         index = key % len(self.map)
         return index
-        
 
     def put(self, key: int, value: int) -> None:
-        index = self.hash(key)
-        self.map[index] = [key,value]
-        
+        index = self.hash(key)  # dummy node
+        head = self.map[index]
+        while head.next:
+            if head.next.key == key:
+                head.next.value = value
+                return
+            head = head.next
+        head.next = NodeList(key, value)
 
     def get(self, key: int) -> int:
         index = self.hash(key)
-        if len(self.map[index]) != 0:
-            return self.map[index][-1]
-        else:
-            return -1
+        head = self.map[index].next
+        while head:
+            if head.key == key:
+                return head.value
+            head = head.next
+        return -1
 
     def remove(self, key: int) -> None:
         index = self.hash(key)
-        if len(self.map[index]) != 0:
-            self.map[index] = []
+        head = self.map[index]
+        prev = head
+        head = head.next
+
+        while head:
+            if head.key == key:
+                prev.next = head.next
+            prev = head
+            head = head.next
 
 
 # Your MyHashMap object will be instantiated and called as such:
