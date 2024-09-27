5c40722685c7c8a66cfe7059f9fd3582cdc8a215
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-28 02:04:17 +0300

    Time: 48 ms (58.82%), Space: 16.7 MB (35.76%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,20 +1,15 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Create a hashmap to store the frequency of elements in nums1
-        count_map = {}
-        for num in nums1:
-            if num in count_map:
-                count_map[num] += 1
+        map = {}
+        for i in nums1:
+            if i in map:
+                map[i] += 1
             else:
-                count_map[num] = 1
-
-        result = []
-
-        # Traverse nums2 and check if the element is in the hashmap
-        for num in nums2:
-            if num in count_map and count_map[num] > 0:
-                result.append(num)
-                count_map[num] -= 1
-
-        return result
+                map[i] = 1
+        res = []
+        for i in nums2:
+            if i in map and map[i] > 0 :
+                res.append(i)
+                map[i] -= 1 
+        return res  
         
