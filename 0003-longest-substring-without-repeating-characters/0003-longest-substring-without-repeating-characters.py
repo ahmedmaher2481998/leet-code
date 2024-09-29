78b5d01afbf82f6ab4dc1e9763d1fa8968c53e6d
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-29 15:01:53 +0300

    Time: 365 ms (6.93%), Space: 16.7 MB (43.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,18 +7,15 @@ class Solution:
         i = 0
         while i < len(string):
             s = string[i]
-            # print(f"{i} {s}")
             if s in map:
                 start += 1
                 i = start
                 main_max = max(main_max, res)
                 res = 0
-                print(map, start)
                 map.clear()
             else:
                 map.add(s)
                 res = res + 1
                 i += 1
-        print("map", map)
-        print("max", main_max)
+
         return max(main_max, len(map))
