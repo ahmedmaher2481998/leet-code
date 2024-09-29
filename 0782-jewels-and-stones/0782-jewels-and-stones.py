6e877a3d4381b123d6762d5f18cada7dd65e120c
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-29 13:30:31 +0300

    Time: 30 ms (87.41%), Space: 16.5 MB (27.6%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,10 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         c = 0
-        jewel_set = set(jewels)
+        # jewel_set = set(jewels)
  
         for s in stones:
-            if s in jewel_set:
+            if s in jewels:
                 c += 1 
         return c
 
