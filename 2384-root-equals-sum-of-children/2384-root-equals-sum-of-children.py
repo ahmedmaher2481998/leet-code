d180fb26371fb039d9fb92acc24c3390fe3509d6
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-01 13:34:38 +0300

    Time: 31 ms (82.55%), Space: 16.4 MB (54.1%) - LeetHub

---
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if not root or not root.left or not root.right:
+            return False
+        return root.val == root.left.val + root.right.val 
+        
