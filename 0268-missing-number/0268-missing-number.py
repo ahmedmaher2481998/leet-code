7d3f706c3467c955996d28d4813c6726f95c77bb
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-05 02:16:59 +0300

    Time: 865 ms (15.79%), Space: 17.8 MB (59.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = 0 
-        while n in nums:
-            n += 1
+        n = len(nums)
+        while n in nums and n >= 0 :
+            n -= 1
         return n 
         
