0cd1c8f6eff5637a9dd568a8cff36a7a1db544ea
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-05 02:08:37 +0300

    Time: 451 ms (85.77%), Space: 30.8 MB (77.69%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,27 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        s = set()
-        for i in nums:
-            if i not in s:
-                s.add(i)
-            else:
-                return i
-        
+        # s = set()
+        # for i in nums:
+        #     if i not in s:
+        #         s.add(i)
+        #     else:
+        #         return i
+        # floyed alogrthim 
+        # i just learn it 
+        # first make fast & slow intercest 
+        # then make new slow and use it to make it intersect with the old slow 
+        # when they intersect , it's  the duplicatye number 
+        fast ,slow =0 ,0
+        while True : 
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast :
+                print(slow , fast)
+                break
+
+        founder = 0
+        while True:
+            founder = nums[founder]
+            slow = nums[slow]
+            if slow == founder:
+                return slow 
