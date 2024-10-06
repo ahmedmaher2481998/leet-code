647b19825b377ca6a2d452616ed3c83c2727ad56
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-07 00:09:15 +0300

    Time: 39 ms (63.37%), Space: 16.7 MB (26.84%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
@@ -1,18 +1,22 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        map = {c: i + 1 for i, c in enumerate(order)}
-        print(map)
-        # loop over worlds find first diff char if next is not greater returen false
-        # if first word finished before second word with no diif cha detected return true
-
+        # first letter same?
+        # continue
+        # :different
+        # next word first letter is latter in order than current word first letter
+        # if this word is suffex of next word it's okay to be in front of it
+        # so if next work is sufex it's okay it's wrong
+        map = {c: i for i, c in enumerate(order)}
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
-
+            # if w2 is suffex of w1 it's not valid
             for j in range(len(w1)):
                 if len(w2) == j:
                     return False
                 if w1[j] != w2[j]:
                     if map[w1[j]] > map[w2[j]]:
                         return False
-                    break
+                    break  # it's legit order no need to continue
+        # print(map)
+
         return True
