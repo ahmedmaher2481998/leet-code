0f64f056604609eff74ae5a19c6920576ec2e951
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-08 14:28:01 +0300

    Time: 59 ms (56.91%), Space: 28.5 MB (38.51%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,22 +1,26 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        l,r=0,10
-        map={}
-        def hash(sub):
-            return "".join(sub)
-        if len(s) < 10:
-            return []
-        while r  <= len(s):
-            sub = hash([s[l:r]])
-            if sub in map:
-                map[sub] += 1
+        # Three's a beter solution using Sets
+        # l,r=0,10
+        # map={}
+        # if len(s) < 10:
+        #     return []
+        # while r  <= len(s):
+        #     sub = s[l:r]
+        #     if sub in map:
+        #         map[sub] += 1
+        #     else:
+        #         map[sub] = 1
+        #     l += 1 
+        #     r += 1
+        # res = []
+        # return [key for key,value in map.items() if value > 1]
+        seen = set()
+        res = set()
+        for i in range(len(s) - 9):
+            sub = s[i:i+10]
+            if sub in seen:
+                res.add(sub)
             else:
-                map[sub] = 1
-            l += 1 
-            r += 1
-        res = []
-        # print(map)
-        return [key for key,value in map.items() if value > 1]
-
-
-        
+                seen.add(sub)
+        return list(res)
