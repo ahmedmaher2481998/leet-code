5d3335e9c0673763424e28816743897846b3f1f7
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-08 15:07:07 +0300

    Time: 84 ms (81.31%), Space: 20.3 MB (53.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         def hash(s:str)->str:
-            a = list(s)
-            print(a) 
-            a.sort()
-            return ''.join(a)
+            # a = list(s)
+            # print(a) 
+            # a.sort()
+            # ''.join(a)
+            return ''.join(sorted(list(s)))
         map = {}
         for s in strs:
             key = hash(s)
