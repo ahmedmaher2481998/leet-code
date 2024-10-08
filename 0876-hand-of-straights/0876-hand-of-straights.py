6c1112d19ee2e783fe46470c202a38ba4414f49d
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-08 14:35:39 +0300

    Time: 168 ms (41.16%), Space: 18.7 MB (9.94%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,8 +1,9 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        h = list(set(hand))
+        h = list(set(hand)) # to trak elements only 
         heapq.heapify(h)
-        map = {}
+        map = {} 
+        # to track frequecny of each element 
         for i in hand:
             if i in map:
                 map[i] += 1
@@ -10,10 +11,10 @@ class Solution:
                 map[i] = 1
         res = []
         while len(h) != 0:
-            i = h[0]
+            i = h[0] # min element 
             if map[i] != 0:
-                t = [i]
                 map[i] -= 1
+                t = [i]
                 for j in range(1, groupSize):
                     new = i  +j 
                     if new in map and map[new] != 0:
