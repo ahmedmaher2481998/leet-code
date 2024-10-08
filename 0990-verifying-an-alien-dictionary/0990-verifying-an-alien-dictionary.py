eb7873acb8eac8d932048e890620a905353af5f2
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-08 14:39:26 +0300

    Time: 46 ms (18.38%), Space: 16.7 MB (27.23%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
@@ -3,20 +3,18 @@ class Solution:
         # first letter same?
         # continue
         # :different
-        # next word first letter is latter in order than current word first letter
-        # if this word is suffex of next word it's okay to be in front of it
-        # so if next work is sufex it's okay it's wrong
+        # next word first letter is greater in order than current word first letter
+        # if this word is suffex of next word it's should to be in front of it
+        # so if next work is sufex then it's wrong
         map = {c: i for i, c in enumerate(order)}
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
-            # if w2 is suffex of w1 it's not valid
             for j in range(len(w1)):
+                # if w2 is suffex of w1 it's not valid
                 if len(w2) == j:
                     return False
-                if w1[j] != w2[j]:
+                if w1[j] != w2[j]: # fpound first diff letter 
                     if map[w1[j]] > map[w2[j]]:
                         return False
                     break  # it's legit order no need to continue
-        # print(map)
-
         return True
