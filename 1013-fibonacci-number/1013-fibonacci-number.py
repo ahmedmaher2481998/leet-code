70e8177a51bb4b93235cd35b15a65ad3ab55ce3d
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-10-15 05:51:59 +0300

    Time: 29 ms (91.52%), Space: 16.7 MB (19.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,18 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    def fib(self, n: int,map={}) -> int:
         if n == 1:
             return 1
         elif n <= 0  :
             return 0
         else:
-            return self.fib(n - 1 ) +  self.fib(n - 2 ) 
-        
+            left = 0
+            right = 0
+            if n -1 in map:
+                left = map[n -1]
+            else:
+                left = map[n - 1] = self.fib(n - 1 ,map)
+            if n -2 in map:
+                right = map[n -2]
+            else:
+                right = map[n - 2 ] = self.fib(n - 2 ,map)
+        return  map[n - 1] + map[n - 2]
